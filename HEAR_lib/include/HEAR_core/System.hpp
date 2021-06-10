9eea7a03140b60415d16f26847cbb990278d7845
AhmedHumais <m.ahmedhumais@gmail.com>
2021-06-10 14:53:38 +0400

    fixing bugs

--- HEAR_lib/include/HEAR_core/System.hpp
+++ HEAR_lib/include/HEAR_core/System.hpp
@@ -133,7 +133,7 @@ void System::connect(OutputPort<T>* op, InputPort<T>* ip){
 
 template <class T>
 void System::connectExternalInput(ExternalInputPort<T>* sys_ip, InputPort<T>* ip){
-    this->connect<T>((sys_ip->getOutputPort<T>(0), ip);
+    this->connect<T>(sys_ip->getOutputPort<T>(0), ip);
 }
 
 template <class T>
