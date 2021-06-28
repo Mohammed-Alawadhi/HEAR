b9fcc5fe125876d91deec97c5b87cd6ff1f5faaf
AhmedHumais <m.ahmedhumais@gmail.com>
2021-06-28 18:43:49 +0400

    fixing kd -ve issue

--- HEAR_lib/src/PID_Block.cpp
+++ HEAR_lib/src/PID_Block.cpp
@@ -56,6 +56,7 @@ void PID_Block::update_params(PID_parameters* para){
     else{
         i_term = false;
     }
+	_parameters.kd = para->kd;
 	// if(para->kd > 0.0){
 	// 	_parameters.kd = para->kd;
     //     d_term = true;
