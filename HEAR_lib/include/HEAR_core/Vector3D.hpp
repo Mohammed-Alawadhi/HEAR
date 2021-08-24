8d0c22cd9699412f198862070aa8d4c1c255b092
AhmedHumais <m.ahmedhumais@gmail.com>
2021-08-24 11:57:58 +0400

    fixed merge conflicts

--- HEAR_lib/include/HEAR_core/Vector3D.hpp
+++ HEAR_lib/include/HEAR_core/Vector3D.hpp
@@ -1,126 +1,3 @@
-
-#ifndef VECTOR3D_HPP
-#define VECTOR3D_HPP
-
-#include <cstdint>
-#include <math.h>
-#include <initializer_list>
-
-namespace HEAR
-{
-
-template  <class L> class Vector2D;
-
-template <typename T>  
-class Vector3D {
-        public:
-                T x = 0;
-                T y = 0;
-                T z = 0;
-                inline Vector3D<T>(){}; 
-                inline Vector3D<T>(const T &x_, const T &y_, const T &z_){
-                        this->x = x_;
-                        this->y = y_;
-                        this->z = z_;
-                }
-                inline friend Vector3D<T> operator + (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        return Vector3D<T>(v1.x+v2.x, v1.y+v2.y, v1.z+v2.z);
-                }
-                inline friend Vector3D<T> operator - (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        return Vector3D<T>(v1.x-v2.x, v1.y-v2.y, v1.z-v2.z);
-                }
-                inline friend Vector3D<T> operator * (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        return Vector3D<T>(v1.x*v2.x, v1.y*v2.y, v1.z*v2.z);
-                }
-                inline friend Vector3D<T> operator * ( const T& tmp, const Vector3D<T> &v1) {
-                        return Vector3D<T>(v1.x*tmp, v1.y*tmp, v1.z*tmp);
-                }
-                inline friend Vector3D<T> operator / (const Vector3D<T>& v1, const T& tmp) {
-                        return Vector3D<T>(v1.x/tmp, v1.y/tmp, v1.z/tmp);
-                }
-                inline Vector3D<T>& operator = (const T& tmp) {
-                        x = tmp;
-                        y = tmp;
-                        z = tmp;
-
-                        return *this;
-                }
-                inline Vector3D<T>& operator = (const std::initializer_list<T>& tmp) {
-                        x = *tmp.begin(); y = *(tmp.begin()+1); z = *(tmp.begin()+2);
-                        return *this;
-                }
-                inline friend bool operator == (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        if(v1.x == v2.x && v1.y == v2.y && v1.z == v2.z) {return true;}
-                        else {return false;}
-                }
-                inline friend bool operator == (const Vector3D<T> &v1, const T& val) {
-                        if(v1.x == val && v1.y == val && v1.z == val) {return true;}
-                        else {return false;}
-                }
-
-                inline friend bool operator != (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        if(v1.x != v2.x || v1.y != v2.y || v1.z != v2.z) {return true;}
-                        else {return false;}
-                }
-                void operator() (const T& x_, const T& y_, const T& z_) {
-                        this->x = x_;
-                        this->y = y_;
-                        this->z = z_;
-                }
-                template <typename M>
-                operator Vector3D<M>() {
-                        Vector3D<M> tmp;
-                        tmp.x = M(this->x);
-                        tmp.y = M(this->y);
-                        tmp.z = M(this->z);
-
-                        return tmp;
-                }
-                inline friend T operator ^ (const Vector3D<T> &v1, const Vector3D<T> &v2) {
-                        return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
-                }
-                inline T dot(const Vector3D<T> &tmp){
-                        return x*tmp.x + y*tmp.y + z*tmp.z;
-                }
-                inline Vector3D<T> cross(const Vector3D<T>& tmp){
-                        return Vector3D<T>(this->y*tmp.z - this->z*tmp.y, this->z*tmp.x - this->x*tmp.z, this->x*tmp.y - this->y*tmp.x);
-                }
-                inline double length(){
-                        return sqrt(dot(*this));
-                }
-                inline Vector3D<double> normalized(){
-                        return Vector3D<double>(*this)/length();
-                }
-
-                static double getL2Norm(Vector3D<T> vec1) {
-                    return sqrt(vec1.x*vec1.x+vec1.y*vec1.y+vec1.z*vec1.z);
-                }
-
-
-                static Vector3D<T> cross(Vector3D<T> vec1,Vector3D<T> vec2) {
-                        Vector3D<T> results;
-                        results.x=vec1.y*vec2.z-vec1.z*vec2.y;
-                        results.y=vec1.z*vec2.x-vec1.x*vec2.z;
-                        results.z=vec1.x*vec2.y-vec1.y*vec2.x;
-                        return results;
-                }
-
-                static double dot(Vector3D<T> vec1,Vector3D<T> vec2) {
-                    return vec1.x*vec2.x+vec1.y*vec2.y+vec1.z*vec2.z;
-                }
-
-                Vector2D<T> project_xy() {
-                        Vector2D<T> res;
-                        res.x=this->x;
-                        res.y=this->y;
-                        return res;
-                }
-};
-
-} 
-
-
-
 #ifndef VECTOR3D_HPP
 #define VECTOR3D_HPP
 
@@ -198,6 +75,11 @@ class Vector3D {
                 inline friend bool operator != (const Vector3D<T> &v1, const Vector3D<T> &v2) {
                         if(v1.x != v2.x || v1.y != v2.y || v1.z != v2.z) {return true;}
                         else {return false;}
+                }
+                void operator() (const T& x_, const T& y_, const T& z_) {
+                        this->x = x_;
+                        this->y = y_;
+                        this->z = z_;
                 }
                 template <typename M>
                 operator Vector3D<M>() {
--- HEAR_lib/include/HEAR_library/Library.hpp
+++ HEAR_lib/include/HEAR_library/Library.hpp
@@ -10,147 +10,6 @@
 #include "HEAR_control/Differentiator.hpp"
 #include "HEAR_control/Eul2Rot.hpp"
 #include "HEAR_control/FbLinearizer.hpp"
-#include "HEAR_control/FromHorizon.hpp"
-#include "HEAR_control/Gain.hpp"
-#include "HEAR_control/HexaActuationSystem.hpp"
-#include "HEAR_control/HoldVal.hpp"
-#include "HEAR_control/InvertedSwitch.hpp"
-#include "HEAR_control/KF3D.hpp"
-#include "HEAR_control/MedianFilter.hpp"
-#include "HEAR_control/Mrft.hpp"
-#include "HEAR_control/Mux3.hpp"
-#include "HEAR_control/PID_Block.hpp"
-#include "HEAR_control/Rot2Eul.hpp"
-#include "HEAR_control/Saturation.hpp"
-#include "HEAR_control/Sum.hpp"
-#include "HEAR_control/Switch.hpp"
-#include "HEAR_control/ToHorizon.hpp"
-#include "HEAR_control/Rot2Quat.hpp"
-#include "HEAR_control/Quat2Rot.hpp"
-
-#include <iostream>
-
-
-namespace HEAR
-{
-
-class Library {
-    public :
-    static Block* createBlock(BLOCK_ID b_type, int b_uid, double _dt = 0, TYPE d_type=TYPE::NA);
-};
-
-Block* Library::createBlock(BLOCK_ID b_type, int b_uid, double _dt, TYPE d_type){
-    Block* blk;
-    switch (b_type)
-    {
-    case BLOCK_ID::BW_FILT2 :
-        if(d_type == TYPE::Float3){
-            blk = new BWFilter2<Vector3D<float>>(b_uid);
-        }
-        else{
-            blk = new BWFilter2<float>(b_uid);
-        }
-        break;
-    case BLOCK_ID::CONSTANT :
-        if(d_type == TYPE::Float3){
-            blk = new Constant<Vector3D<float>>(b_uid);
-        }
-        else{
-            blk = new Constant<float>(b_uid);
-        }
-        break;
-    case BLOCK_ID::DEMUX3 :
-        blk = new Demux3(b_uid);
-        break;
-    case BLOCK_ID::DIFFERENTIATOR :
-        if(d_type == TYPE::Float3){
-            blk = new Differentiator<Vector3D<float>>(_dt, b_uid);
-        }
-        else{
-            blk = new Differentiator<float>(_dt, b_uid);
-        }
-        break;
-    case BLOCK_ID::EUL2ROT :
-        blk = new Eul2Rot(b_uid);
-        break;
-    case BLOCK_ID::FORCE2ROT :
-        blk = new FbLinearizer::Force2Rot(b_uid);
-        break;
-    case BLOCK_ID::FROMHORIZON :
-        blk = new FromHorizon(b_uid);
-        break;
-    case BLOCK_ID::GAIN :
-        blk = new Gain(b_uid);
-        break;
-    case BLOCK_ID::HEXAACTUATIONSYSTEM :
-        blk = new HexaActuationSystem(b_uid);
-        break;
-    case BLOCK_ID::HOLDVAL :
-        blk = new HoldVal(b_uid);
-        break;
-    case BLOCK_ID::INVERTED_SWITCH :
-        blk = new InvertedSwitch(b_uid);
-        break;
-    case BLOCK_ID::KF :
-        blk = new KF3D(b_uid, _dt);
-        break;
-    case BLOCK_ID::MEDIAN_FILTER :
-        blk = new MedianFilter(b_uid);
-        break;
-    case BLOCK_ID::MRFT :
-        blk = new MRFT_Block(b_uid);
-        break;
-    case BLOCK_ID::MUX3 :
-        blk = new Mux3(b_uid);
-        break;
-    case BLOCK_ID::PID :
-        blk = new PID_Block(_dt, b_uid);
-        break;
-    case BLOCK_ID::QUAT2ROT :
-        blk = new Quat2Rot(b_uid);
-        break;
-    case BLOCK_ID::ROT2EUL :
-        blk = new Rot2Eul(b_uid);
-        break;
-    case BLOCK_ID::ROT2QUAT :
-        blk = new Rot2Quat(b_uid);
-        break;
-    case BLOCK_ID::ROTDIFF2ROD :
-        blk = new FbLinearizer::RotDiff2Rod(b_uid);
-        break;
-    case BLOCK_ID::SATURATION :
-        blk = new Saturation(b_uid);
-        break;
-    case BLOCK_ID::SUM :
-        blk = new Sum(b_uid);
-        break;
-    case BLOCK_ID::SWITCH :
-        blk = new Switch(b_uid);
-        break;
-    case BLOCK_ID::TOHORIZON:
-        blk = new ToHorizon(b_uid);
-        break; 
-    default:
-        std::cout << "Invalid BlockID \n";
-        return NULL;
-    }
-    return blk;
-}
-
-}
-
-#ifndef LIBRARY_HPP
-#define LIBRARY_HPP
-
-#include "HEAR_core/Block.hpp"
-#include "HEAR_core/Vector3D.hpp"
-
-#include "HEAR_control/BWFilter.hpp"
-#include "HEAR_control/Constant.hpp"
-#include "HEAR_control/Demux3.hpp"
-#include "HEAR_control/Differentiator.hpp"
-#include "HEAR_control/Eul2Rot.hpp"
-#include "HEAR_control/FbLinearizer.hpp"
 #include "HEAR_control/FoH.hpp"
 #include "HEAR_control/FromHorizon.hpp"
 #include "HEAR_control/Gain.hpp"
@@ -158,6 +17,7 @@ Block* Library::createBlock(BLOCK_ID b_type, int b_uid, double _dt, TYPE d_type)
 #include "HEAR_control/HoldVal.hpp"
 #include "HEAR_control/InvertedSwitch.hpp"
 #include "HEAR_control/InvertedSwitch3.hpp"
+#include "HEAR_control/KF3D.hpp"
 #include "HEAR_control/MedianFilter.hpp"
 #include "HEAR_control/Mrft.hpp"
 #include "HEAR_control/Multiply.hpp"
@@ -244,6 +104,9 @@ Block* Library::createBlock(BLOCK_ID b_type, int b_uid, double _dt, TYPE d_type)
         break;
     case BLOCK_ID::INVERTED_SWITCH3 :
         blk = new InvertedSwitch3(b_uid);
+        break;
+    case BLOCK_ID::KF :
+        blk = new KF3D(b_uid, _dt);
         break;
     case BLOCK_ID::MEDIAN_FILTER :
         blk = new MedianFilter(b_uid);
