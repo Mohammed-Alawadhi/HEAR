e01854b31b66eee2e8355ca53ab3527618afbfa8
AhmedHumais <m.ahmedhumais@gmail.com>
2021-09-22 15:50:16 +0400

    fixed bug with optiNodelet

--- HEAR_lib/HEAR_nodelet/OptiNodelet.cpp
+++ HEAR_lib/HEAR_nodelet/OptiNodelet.cpp
@@ -24,8 +24,12 @@ namespace HEAR
         sys->connectExternalInput(pos_port, opti_port[0]);
         sys->connectExternalInput(ori_port, opti_port[1]);
 
+        auto diff_pos = sys->createBlock(BLOCK_ID::DIFFERENTIATOR, "Pos_Derivative", TYPE::Float3); ((Differentiator<Vector3D<float>>*)diff_pos)->supPeak(0.3);
+        sys->connectExternalInput(pos_port, diff_pos->getInputPort<Vector3D<float>>(0));
+        
         // connect publishers to KF output ports
         sys->createPub<Vector3D<float>>(TYPE::Float3, "/opti/pos", ((Block*)pos_port)->getOutputPort<Vector3D<float>>(0));
+        sys->createPub<Vector3D<float>>(TYPE::Float3, "/opti/vel", diff_pos->getOutputPort<Vector3D<float>>(0));
         sys->createPub<Vector3D<float>>(TYPE::Float3, "/opti/ori", ((Block*)ori_port)->getOutputPort<Vector3D<float>>(0));
 
         sys->start();
